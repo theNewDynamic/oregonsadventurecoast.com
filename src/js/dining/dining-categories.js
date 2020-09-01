f978adc7f669bde7eca847b297eb11181854aeae
mountainbug95 <tannersmithtn95@gmail.com>
2020-08-31 19:41:46 -0500

    Fix bug in displaying dining categories

--- src/js/dining/dining-categories.js
+++ src/js/dining/dining-categories.js
@@ -1,6 +1,6 @@
 export const entryCategoryOptions = [
-    {value: 1, label: 'Hotels, Motels & Inns', class: ''},
-    {value: 2, label: 'RV Parks & Camping', class: ''},
-    {value: 3, label: 'Bed & Breakfasts', class: ''},
-    {value: 4, label: 'Vacation Rental Homes', class: ''}
+    {value: 1, label: 'American', class: ''},
+    {value: 2, label: 'Asian', class: ''},
+    {value: 3, label: 'BBQ', class: ''},
+    {value: 4, label: 'Bakery', class: ''}
 ];
